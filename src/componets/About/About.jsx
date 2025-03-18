892e0dc5a5a05bde0f8bcafb449a5441588ef64c
Daniel Garcia <61481194+danny-garxia@users.noreply.github.com>
2025-03-18 15:16:20 -0700

    Update About.jsx

--- src/componets/About/About.jsx
+++ src/componets/About/About.jsx
@@ -13,14 +13,15 @@ export default function About() {
           
           {/* Description */}
         <p className={styles.aboutDesc}>
-          I Am A Former Google Intern With A Solid Foundation In Computer Science, 
-          Having Earned My Associate's Degree From Mt. San Jacinto Community College And
-          My Bachelor's Degree From California State University, Fullerton. My Academic Journey 
-          Has Equipped Me With Comprehensive Knowledge In Various Areas, Including Cybersecurity,
-          Computer Networking, Network Security, Web Development, And App Development. This Diverse 
-          Skill Set Has Not Only Made Me A Proficient Full-Stack Developer But Also Provided Me With Essential
-          Expertise In Fundamental Security Practices. I Am Passionate About Leveraging Technology To Create Innovative
-          Solutions And Continuously Expanding My Technical Abilities.
+         I am a former Google intern with a strong foundation in computer science, holding an 
+          associate's degree from Mt. San Jacinto Community College 
+          and a bachelor's degree from California State University, Fullerton. 
+          My academic journey has provided me with comprehensive knowledge in cybersecurity, 
+          computer networking, network security, web development, and app development. 
+          This diverse skill set has shaped me into a proficient full-stack developer while
+          equipping me with essential expertise in fundamental security practices.
+          I am passionate about leveraging technology to develop innovative solutions and 
+          continuously expanding my technical abilities.
         </p>
     </div>
     
